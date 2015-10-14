40ede14ba650088d9ed9fa8b9e4a176d98553b36
sobuj <sobuj.ah@gmail.com>
2015-10-14 23:41:56 +0600

    Data Service

---
+++ Formigination_DataService/ProfileDataService.cs
@@ -0,0 +1,17 @@
+﻿using Formigination_DataService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formigination_DataService
+{
+    public class ProfileDataService
+    {
+        public string SaveProfile(Profile objProfile)
+        {
+            return "";
+        }
+    }
+}
