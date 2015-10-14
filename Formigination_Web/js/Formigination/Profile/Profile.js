fe4cf668b4fa57fe23a2038fb1fee7c83c3b63c1
MD. Mehedi Hasan <bdmehedi2015@outlook.com>
2015-10-14 23:29:58 +0600

    Profile Entity

---
+++ Formigination_Web/js/Formigination/Profile/Profile.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("#btnSave").click(function () {
+        profileManager.saveProfileInfo();
+    });
+});
+
+var profileManager = {
+    saveProfileInfo: function () {
+        var jsonParam = 'user=' + login + '&pass=' + pass;
+        var serviceUrl = "Service1.svc/Login/";
+        AjaxManager.GetJson(serviceUrl, jsonParam, function (data) { alert(data); }, function (data) { alert(data); });
+
+    }
+};
+
+var profileHelper = {
+
+};
