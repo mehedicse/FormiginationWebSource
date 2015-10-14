5cbdb6dba73b47b925d72ddaa2d96c66f49b1331
MD. Mehedi Hasan <bdmehedi2015@outlook.com>
2015-10-14 23:30:27 +0600

    Profile Entity
    dvsdv

---
+++ Formigination_DataService/Entities/Profile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formigination_DataService.Entities
+{
+    public class Profile
+    {
+        public int ProfileId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public int Gender { get; set; }
+        public int MeritalStatus { get; set; }
+        public string BloodGroup { get; set; }
+        public string Religious { get; set; }
+        public string Nationality { get; set; }
+        public string NationalId { get; set; }
+        public string BirthCertificate {get;set; }
+        public string PassportNo { get; set; }
+        public string SocialId { get; set; }
+        public string About { get; set; }
+
+
+    }
+}
